cb13d6a286190f79dcf8874fd8142fa56727d9b0
svx5109 <svx5109@psu.edu>
2024-12-07 03:11:24 -0500

    client and server chat for localhost

---
+++ tcp_client.c
@@ -0,0 +1,132 @@
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define SERV_TCP_PORT 23 /* server's port */
+#define MAX_SIZE 80
+
+static void* readSockFd(void *arg)
+{	
+	int *sockfd = (int *) arg;
+		
+	for(;;)
+	{
+		char buf[MAX_SIZE];
+		int len;
+		
+		len = read(*sockfd, buf, MAX_SIZE);
+		
+		if (len <= 0)
+		{
+			perror("The server shut down");
+			return 0;
+		}
+		else{
+			buf[len] = 0;
+			printf("%s", buf);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, returnsockfd, newsockfd;
+    char *serv_host = "localhost";
+    struct hostent *host_ptr;
+    int port, len, s;
+    int buff_size = 0;
+    int opt = 1;
+	char string[MAX_SIZE];
+    struct sockaddr_in cli_addr, serv_addr;
+	pthread_t thread1;
+
+    /* command line: client [host [port]]*/
+  
+	//1. get host
+	if(argc >= 2)
+        serv_host = argv[1]; /* read the host if provided */
+	
+	printf("selected host: %s \n", serv_host);
+
+	//2. get port
+    if(argc == 3)
+        sscanf(argv[2], "%d", &port); /* read the port if provided */
+    else
+        port = SERV_TCP_PORT;
+
+	printf("Selected port: %d \n", port);
+
+    // 3. get the address of the host
+    if((host_ptr = gethostbyname(serv_host)) == NULL)
+    {
+        perror("gethostbyname error");
+        exit(1);
+    }
+
+    if(host_ptr->h_addrtype !=  AF_INET)
+    {
+        perror("unknown address type");
+        exit(1);
+    }
+
+	//4. bind server address
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = ((struct in_addr *)host_ptr->h_addr_list[0])->s_addr;
+    serv_addr.sin_port = htons(port);
+	
+    //5. open a TCP socket
+    if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("can't open stream socket");
+        exit(1);
+    }
+
+    //6. connect to the server
+    //arg1: sockfd
+    //arg2: server address
+    //arg3: length
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("can't connect to server");
+        exit(1);
+    }
+	
+	
+	//separate thread waits for messages from the client socket
+	//allocate memory
+	printf("attempting to create thread: %d\n", sockfd);
+
+	int *arg = malloc(sizeof(*arg));
+	if ( arg == NULL ) {
+		fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+		exit(EXIT_FAILURE);
+	}
+	*arg = sockfd;
+	
+	//create thread
+	s = pthread_create(&thread1, NULL, readSockFd, arg);
+	if (s != 0)
+	{
+		perror("Thread create error");
+	}	
+
+    //writing messages to server 
+	for(;;)
+	{
+		char msg[MAX_SIZE];
+		
+		printf("Write message here: ");
+		fgets(msg, sizeof(msg), stdin);
+		write(sockfd, msg, sizeof(msg));
+	}
+}
---
+++ tcp_server.c
@@ -0,0 +1,149 @@
+
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+
+#define SERV_TCP_PORT 23 // server's port number
+#define MAX_SIZE 80
+
+int sockTable[10];
+
+void writeAll(char msg[], int size)
+{
+	printf("msg: %s\n", msg);
+	for (int i = 4; i < size; i++)
+	{
+		write(i, msg, size);
+	}
+}
+
+static void * readSockFd(void* arg)
+{
+	int *clisockfd = (int *) arg;
+	
+	printf("now reading socket fd %d\n", *clisockfd);
+	
+	for (;;)
+    {
+        char string[MAX_SIZE];
+		int len;
+
+		//6. READ MESSAGE
+        /* read message from client*/
+        //read newsockfd into string
+        len = read(*clisockfd, string, MAX_SIZE);
+		
+		//printf("len: %d\n", len);
+		
+		if(len <= 0)
+		{
+			printf("User %d has disconnected\n", *clisockfd);
+			//sockTable[*clisockfd] = NULL;
+			close(*clisockfd);
+			pthread_exit(0);
+		}
+		else
+		{
+			/* make sure its a proper string */
+			string[len] = 0;
+			printf("%s\n", string);
+			
+			//send message to other clients
+			printf("sending message to other clients \n");				
+			writeAll(string, sizeof(string));
+		}
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, clilen;
+    struct sockaddr_in cli_addr, serv_addr;
+    int port;
+    char string[MAX_SIZE];
+    int len;
+    int opt = 1;
+	
+    /* command line: server [port_number]*/
+
+    //1. CHOOSING PORT
+    if(argc == 2)
+        //read port is provided, then use the port number
+        sscanf(argv[1], "%d", &port);
+    else
+        //if port is not provided, use port 23
+        port = SERV_TCP_PORT;
+	
+	printf("Selected port: %d \n", port);
+
+
+    //2. OPENING SOCKET
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+    {
+        perror("can't open stream socket");
+        exit(1);
+    }
+
+    // Set socket options
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(1);
+    }
+	
+	printf("opened socket: %d \n" , sockfd);
+
+    //3. BINDING ADDRESS
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(port);
+
+    if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("can't bind local address");
+        exit(1);
+    }
+
+    //4. LISTEN TO SOCKET
+    listen(sockfd, 5);
+
+	//clilen = sizeof(cli_addr);
+	//newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+	//5. ACCEPT NEW CONNECTIONS
+	for(;;)
+	{
+		struct sockaddr_in cli_addr;
+		int clisockfd, clilen, s;
+		pthread_t thread1;
+
+		//accept
+		clilen = sizeof(cli_addr);
+		clisockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		sockTable[sockfd] = sockfd;
+		
+		printf("User %d has joined\n", clisockfd);
+
+		//separate thread waits for messages from the client socket
+		int *arg = malloc(sizeof(*arg));
+        if ( arg == NULL ) {
+            fprintf(stderr, "Couldn't allocate memory for thread arg.\n");
+            exit(EXIT_FAILURE);
+        }
+		*arg = clisockfd;
+		
+		s = pthread_create(&thread1, NULL, readSockFd, arg);
+		
+		if (s != 0)
+		{
+			perror("Thread create error");
+		}	
+	}
+}
